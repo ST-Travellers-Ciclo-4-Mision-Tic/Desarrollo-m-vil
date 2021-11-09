07b927ede341a135e7cd672a0fa08549469f3a4f
AlfonsHoz <alfonsleo123@gmail.com>
2021-11-09 17:11:37 -0500

    Agregando el main, una vista de un poi y algod e funcionalidad para ver desde le main un poi

--- app/src/main/java/com/example/st_travellers/MainActivity.kt
+++ app/src/main/java/com/example/st_travellers/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.st_travellers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,23 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnAdd: TextView = findViewById(R.id.btnAdd)
-        val btnMinus: TextView = findViewById(R.id.btnMinus)
-        val btnReset: TextView = findViewById(R.id.btnReset)
-        val text: TextView = findViewById(R.id.resultado)
-        var sum = 0
-
-        btnAdd.setOnClickListener(View.OnClickListener {
-            sum += 1
-            text.text = sum.toString()
-        })
-        btnMinus.setOnClickListener(View.OnClickListener {
-            sum -= 1
-            text.text = sum.toString()
-        })
-        btnReset.setOnClickListener(View.OnClickListener {
-            sum = 0
-            text.text = sum.toString()
-        })
+        val btnVerMas = findViewById<Button>(R.id.btnVerMas)
+        btnVerMas.setOnClickListener{
+            val intent = Intent(this, PoiActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/st_travellers/PoiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.st_travellers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PoiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.poi_activity)
+    }
+}
