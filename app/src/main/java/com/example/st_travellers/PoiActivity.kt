ce3a4a0f166b85601fcbc2182fc458538b340b23
ismaelc511 <ismaelcarvajal@outlook.com>
2021-11-14 13:32:53 -0500

    me permito subir modificaciones del Constrait Layout, del font-family y agregar propiedad para justificar la descripción

--- app/src/main/java/com/example/st_travellers/PoiActivity.kt
+++ app/src/main/java/com/example/st_travellers/PoiActivity.kt
@@ -1,11 +1,18 @@
 package com.example.st_travellers
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class PoiActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.poi_activity)
+
+        val tituloHeader = findViewById<TextView>(R.id.titleHeader)
+        tituloHeader.text = "TÍTULO GENÉRICO"
     }
 }
